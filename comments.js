e9a7d57de7b8d63930a058af3b8edab77a3a7657
Francis <fran6.desbiens@gmail.com>
2024-08-01 00:36:54 +0000

    Copilot third(ish) commit

--- comments.js
+++ comments.js
@@ -1,12 +1,7 @@
 // Create Web server
-// Create a server that listens on port 3000 and responds with the content of the file comments.html
-const http = require('http');
-const fs = require('fs');
-
-http.createServer((req, res) => {
-  fs.readFile('comments.html', (err, data) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(data);
-    res.end();
-  });
-}).listen(3000);
+import express from 'express';
+const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('Server is running at http://localhost:3000');
+});
